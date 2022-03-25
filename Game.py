d3feb2a0fe0a4c44423529546747235882b8a4e9
Mattéo Pappalardo <mat.papp@free.fr>
2022-03-25 12:40:57 +0100

    game start : hands generation, player sync

--- Game.py
+++ Game.py
@@ -1,34 +1,40 @@
-from multiprocessing import Lock
+from multiprocessing import Event, Lock
 from multiprocessing.managers import BaseManager
+import random
+import sys
 import sysv_ipc
 
-"""
-Cambiecolo is the environmentalist cousin of the Cambio card game. Its goal is presenting a hand of 5
-cards of the same transport means. The player who succeeds is awarded the points of the transport they
-put together. The game deals as many different types of transports as there are players. Possible transport
-means are: airplane, car, train, bike and shoes. Each player receives 5 random cards, face down, e.g. if there
-are 3 players, 15 cards of 3 transport means are distributed, 5 cards per transport. A bell is placed in the
-middle of the players. As soon as cards are distributed, players start exchanging by announcing the
-number of cards they offer, from 1 to 3 identical cards, without showing them. They exchange the same
-number of cards with the first player to accept the offer. This continues until one of the players rings the
-bell and presents a hand of 5 identical cards, scoring the points of the transport they have grouped.
-
-Cambiecolo involves 2 types of processes:
-• game: implements the game session, keeping track of current offers and the bell.
-• player: interacts with the user 1, the game process and other player processes keeping track of
-and displaying the hand and current offers. Interactions with the game process are carried out in a
-separate thread.
-Inter-process communication: Current offers are stored in a shared memory accessible to player
-processes to update their own offer as well as to check current offers. Communication between player
-processes takes place in a message queue requiring a carefully designed exchange protocol.
-"""
+T = ["airplane", "car", "train", "bike", "shoes"]
+Transports = range(len(T))
 
 
 class Shm():
+    hands_start: list[list[int]] = []
+    playersPID: list[int] = []
     offers: list[int] = []
-    lock: Lock = Lock()
+    lock = Lock()
+    start_game = Event()
 
-    def get_offers(self):
+    def wait_for_start(self):
+        self.start_game.wait()
+
+    def get_hand(self, pid) -> tuple[list[int], int]:
+        with self.lock:
+            if len(self.hands_start) == 0:
+                return (None, None)
+
+            self.playersPID.append(pid)
+            i = len(self.playersPID) - 1
+            hand = self.hands_start.pop()
+            hand.sort()
+
+            # launch game if all players are ready
+            if len(self.hands_start) == 0:
+                self.start_game.set()
+
+            return (hand, i)
+
+    def get_offers(self) -> list[int]:
         with self.lock:
             return self.offers
 
@@ -41,15 +47,25 @@ class Game():
     manager: BaseManager
     shm: Shm = Shm()
     exchanges: sysv_ipc.MessageQueue
+    nb_players: int
 
-    def __init__(self):
+    def __init__(self, nb_players: int):
+        self.nb_players = nb_players
+
         self.exchanges = sysv_ipc.MessageQueue(666, sysv_ipc.IPC_CREAT)
 
         BaseManager.register("shm", lambda: self.shm)
         self.manager = BaseManager(address=("127.0.0.1", 6666), authkey=b"cambiecolo")
 
-        print(self.exchanges)
-        print(self.manager)
+        # Hands generation
+        possible_transports = list(Transports)[nb_players - 1:]
+        print("Possible transports:", possible_transports)
+        cards = possible_transports * 5
+        print("Cards pool         :", cards)
+        random.shuffle(cards)
+        print("Cards pool shuffled:", cards)
+        self.shm.hands_start = [cards[i:i + 5] for i in range(0, len(cards), 5)]
+        print("Cards:", self.shm.hands_start)
 
     def stop(self):
         print("Stopping game")
@@ -60,11 +76,24 @@ class Game():
         # Start game
         self.manager.start()
 
+        print("Waiting for all {} players to get ready...".format(self.nb_players))
+        self.shm.start_game.wait()
+
+        print("Game started")
+
         while True:
             pass
 
 
 if __name__ == "__main__":
-    g = Game()
+    try:
+        n = int(sys.argv[1])
+        if not 1 <= n <= 5:
+            raise ValueError
+    except:
+        print("Usage: {} <nb_players [1;5]>".format(sys.argv[0]))
+        exit()
+
+    g = Game(n)
     g.start()
     g.stop()
--- Player.py
+++ Player.py
@@ -1,11 +1,12 @@
+import os
 import sys
 import sysv_ipc
 from multiprocessing.managers import BaseManager
 
 from Game import Shm
 
-TransportsStr = ["airplane", "car", "train", "bike", "shoes"]
-Transports = range(len(TransportsStr))
+T = ["airplane", "car", "train", "bike", "shoes"]
+Transports = range(len(T))
 
 
 class Player():
@@ -13,14 +14,13 @@ class Player():
     exchanges: sysv_ipc.MessageQueue
     shm: Shm
 
+    i: int
     hand: list[int] = []
     offer: int = None
     # offer contains transport type
     # when annoucing an offer, send hand.count(offer)
 
-    def __init__(self, i):
-        self.i = i
-
+    def __init__(self):
         self.exchanges = sysv_ipc.MessageQueue(666)
 
         BaseManager.register("shm")
@@ -29,34 +29,69 @@ class Player():
 
         self.shm = self.manager.shm()
 
-        print(self.exchanges)
-        print(self.manager)
+        (self.hand, self.i) = self.shm.get_hand(os.getpid())
+        if self.hand is None:
+            print("Game already started")
+            sys.exit(0)
+
+    def display(self):
+        print("Player {}".format(self.i))
+        print("Hand:", " ".join([T[c] + " (" + str(c) + ") " for c in self.hand]))
+        print("Currnet offer: {}".format(self.offer))
+
+    def input_int(self, s) -> int:
+        a = input(s + " > ")
+        try:
+            return int(a)
+        except ValueError:
+            print("Not an integer")
+            return self.input_int(s)
 
     def start(self):
         # Start player
 
+        print("Waiting for game to start...")
+        self.shm.wait_for_start()
+
         while True:
+            self.display()
+
+            choice = self.input_int("Choose a transport id to offer " + str(set(self.hand)))
+            #Todo
+
+            self.check_queue()
             pass
 
     def stop(self):
         self.manager.close()
 
-    def exchange(self):
+    def try_announce(self):
+        # announce offer
+        # if offer is possible, announce it
+
+        self.shm.offer(self.i, self.hand.count(self.offer))
+
+    def try_exchange(self):
+        # Check hand for exchange
+        # if exchange is possible, offer it
+        pass
+
+    def check_queue(self):
         # Wait for exchange (polling, signals maybe?)
         try:
             msg = self.exchanges.receive(block=False)
         except:
             return
 
-        # someone offers something (i, nb_cards, card_type)
+        # someone offers something ("hey", their_i, nb_cards, card_type)
         # if i can accept, accept:
         # add cards to hand
-        # send cards to other player (i, nb_cards, card_type)
+        # send cards to other player (self.i, nb_cards, card_type)
 
-        # if i can't accept, send nack (i)
+        # if i can't accept, send nack ("nope", self.i)
 
 
 if __name__ == "__main__":
-    p = Player(int(sys.argv[1]))
+    p = Player()
     p.start()
     p.stop()
